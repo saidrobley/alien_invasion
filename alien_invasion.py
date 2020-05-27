93678cc8b64b5df426555a244f44af443489fdff
Said Robley <saidrobley@gmail.com>
2020-05-27 12:09:22 -0600

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,38 @@
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+
+
+def run_game():
+    # initialize game and create a screen object
+    pygame.init()
+
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+
+    pygame.display.set_caption("Alien Invasion")
+    bg_color = (230, 230, 230)
+
+    # Make a ship
+    ship = Ship(ai_settings, screen)
+
+    # Make a group to store bullets in.
+    bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
+
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+
+run_game()
